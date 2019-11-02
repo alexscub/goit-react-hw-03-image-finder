a65ea4db1164f6af1862418a9f7521a77b87e2cb
alexscub <alexscub1@gmail.com>
2019-11-02 19:16:27 +0200

    Updates

---
+++ precache-manifest.5fae6e1a1ba44256ed288252fd82c25e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f3b0b80b2db1ac831da0d5eaed1fcc",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "198702c8badfbba9bd51",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.54cc9712.chunk.css"
+  },
+  {
+    "revision": "590695edf5a310d62790",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.8df665e4.chunk.js"
+  },
+  {
+    "revision": "198702c8badfbba9bd51",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.cc3fbf9e.chunk.js"
+  },
+  {
+    "revision": "1cf333cb676ecbf70095",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ef1ef8b1.js"
+  }
+]);
